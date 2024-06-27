0f82469092a133ed249769aeba61a4458e0c0847
MichaelXcX <ciuciuletemihaicm@gmail.com>
2024-06-27 19:06:40 +0300

    Lab2 almost done

--- Curs2/check-balance.ts
+++ Curs2/check-balance.ts
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { airdropIfRequired } from "@solana-developers/helpers";
 
 const connection = new Connection(clusterApiUrl("devnet"));
 
 const my_key = "GBiGhtVYwM6d7XNgoYxgh1uEeDitpKPsRtVcDEPJj43z";
 const publicKey = new PublicKey(my_key);
 
+airdropIfRequired(connection, publicKey);
 
 const blanceInLamports = await connection.getBalance(publicKey);
 
 console.log("Connected to cluster:", clusterApiUrl("devnet"));
-console.log("Balance in lamports:", blanceInLamports / LAMPORTS_PER_SOL, "LAMPORTS");
+console.log("Balance in lamports:", blanceInLamports, "LAMPORTS");
+console.log("Balance in SOL:", blanceInLamports / LAMPORTS_PER_SOL, "SOL");
