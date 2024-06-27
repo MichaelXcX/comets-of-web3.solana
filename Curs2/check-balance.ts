6ac1ebe6809a627e723cdd10b423483a3d36755f
MichaelXcX <ciuciuletemihaicm@gmail.com>
2024-06-27 19:52:07 +0300

    Done with wallet too

--- Curs2/check-balance.ts
+++ Curs2/check-balance.ts
@@ -1,15 +1,24 @@
 import "dotenv/config";
 import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
-import { airdropIfRequired } from "@solana-developers/helpers";
+import { airdropIfRequired, getKeypairFromEnvironment } from "@solana-developers/helpers";
 
+import bs58 from "bs58";
+
+// Ma conectez la clusterul devnet
 const connection = new Connection(clusterApiUrl("devnet"));
 
-const my_key = "GBiGhtVYwM6d7XNgoYxgh1uEeDitpKPsRtVcDEPJj43z";
-const publicKey = new PublicKey(my_key);
+const myKey = "GBiGhtVYwM6d7XNgoYxgh1uEeDitpKPsRtVcDEPJj43z";
+const publicKey = new PublicKey(myKey);
 
-airdropIfRequired(connection, publicKey);
+//  !!!airdropIfRequired e async!!! (face un request, e obvious)
+await airdropIfRequired(connection, publicKey, 2 * LAMPORTS_PER_SOL, 0.5 * LAMPORTS_PER_SOL);
 
+// Same here legat de async, dar e mai evident
 const blanceInLamports = await connection.getBalance(publicKey);
+
+const privateKey = getKeypairFromEnvironment("SECRET_KEY");
+console.log("Private key:", bs58.encode(privateKey.secretKey));
+
 
 console.log("Connected to cluster:", clusterApiUrl("devnet"));
 console.log("Balance in lamports:", blanceInLamports, "LAMPORTS");
